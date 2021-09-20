478cbac4bfbb5c55397bb437c5f3bb177d1c9d3e
unknown <izzy.zammil@gmail.com>
2021-09-20 16:25:48 +0700

    Completed parts Testimonial + Footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,9 @@ import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
+
 import landingPage from "json/landingPage.json";
 
 export default class LandingPage extends Component {
@@ -21,6 +24,8 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
